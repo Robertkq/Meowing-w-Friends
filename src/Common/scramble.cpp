bb63e68400ea32092bd56dab86fd5d76d608530c
Robert Vuia <robertidfk69@gmail.com>
2022-11-02 01:25:30 +0200

    Removed scramble files

--- src/Common/scramble.cpp
+++
@@ -1,6 +0,0 @@
-#include "scramble.h"
-
-uint64_t scramble(uint64_t num)
-{
-    return ((num ^ 0x5A9B6C2F0F011) & 0xF0F0F0F0F0F0F0) >> 3;
-}
--- src/Common/scramble.h
+++
@@ -1,8 +0,0 @@
-#ifndef KQMWFSCRAMBLE
-#define KQMWFSCRAMBLE
-
-#include <cstdint>
-
-uint64_t scramble(uint64_t);
-
-#endif
